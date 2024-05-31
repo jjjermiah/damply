8ed31c972b3ac76f4def1b02bb50c6fee84a2172
Jermiah Joseph <jermiahjoseph98@gmail.com>
2024-05-31 11:45:00 -0400

    feat: first commit

---
+++ src/damply/cli.py
@@ -0,0 +1,9 @@
+from rich import print
+import rich_click as click
+
+
+@click.command()
+@click.option("--name", type=str, prompt="What is your name?")
+def cli(name: str):
+    """Simple CLI that greets NAME for a total of COUNT times."""
+    print(f"Hello, {name}!")
