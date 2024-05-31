569177b476047cadb22ae84d0eeb92ad1fb370ea
Jermiah Joseph <jermiahjoseph98@gmail.com>
2024-05-31 13:03:01 -0400

    feat: add tests and major class

--- src/damply/cli.py
+++ src/damply/cli.py
@@ -1,12 +1,13 @@
-from rich import print
-import rich_click as click
+from typing import Literal
 
+import rich_click as click
+from rich import print
 
 @click.command()
 @click.option("--name", type=str, prompt="What is your name?")
-def cli(name: str):
+def cli(name: str) -> None:
     """Simple CLI that greets NAME for a total of COUNT times."""
     print(f"Hello, {name}!")
 
-def hello():
+def hello() -> Literal['Hello, World!']:
     return "Hello, World!"
