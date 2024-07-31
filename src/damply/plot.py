3e94e4aff321a5b4fec8e388084565609294b56a
jjjermiah <jermiahjoseph98@gmail.com>
2024-07-31 12:00:43 -0400

    fix: fix arrangement

--- src/damply/plot.py
+++ src/damply/plot.py
@@ -1,11 +1,10 @@
-import os
-from dataclasses import dataclass
 from pathlib import Path
 from typing import Any, Dict, List
 import datetime
 import pandas as pd
 import plotly.graph_objects as go
-import rich_click as click
+
+from damply.utils import Directory, DirectoryList
 
 """This is a sandbox script to plot a sankey diagram of the dmp"""
 
@@ -23,58 +22,13 @@ Goal:
 """
 
 
-@dataclass
-class Directory:
-    directory: Path
-    size_GB: int
-
-    def __post_init__(self):
-        self.parent = self.directory.parent
-
-    def __getitem__(self, key: str) -> Any:
-        return self.__dict__[key]
-
-    def __repr__(self):
-        return f"Directory({self.directory}, {self.size_GB})"
-
-
-@dataclass
-class DirectoryList:
-    directories: List[Directory]
-
-    def __post_init__(self):
-        self.common_root = self.get_common_root()
-
-    def get_common_root(self) -> Path:
-        return Path(
-            os.path.commonprefix(
-                [str(directory.directory) for directory in self.directories]
-            )
-        )
-
-    def __len__(self) -> int:
-        return len(self.directories)
-
-    def __getitem__(self, key: int) -> Directory:
-        return self.directories[key]
-
-    def __repr__(self):
-        fmt_str = ""
-        fmt_str += f"CommonPre:{self.common_root}\n"
-        for directory in self.directories:
-            fmt_str += f"{directory}\n"
-        return fmt_str
-
-    def dir_size_dict(self) -> Dict[Path, int]:
-        return {
-            directory.directory: directory.size_GB for directory in self.directories
-        }
-
 
 def permutate_path(path: Path) -> List[Path]:
     """
     Given a path, return all possible paths from the root to the path
     """
+    assert path.is_absolute(), "The path must be absolute"
+    
     nodes = []
     while path != Path("/"):
         nodes.append(path)
